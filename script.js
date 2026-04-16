20e2153e0ad9ffe1b9b62729cb9dbff7846cd654
poran-dip <porandip0@gmail.com>
2026-04-16 19:13:15 +0530

    feat: minimize copied code

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 const copyBtn = document.querySelector(".copy");
 
-const code = `<!doctype html>
-<html>
-  <head>
-    <title>Animated Gradient</title>
-    <style>
-      body {
-        min-height: 100vh;
-      }
+const code = `<style>
+  .animated-gradient {
+    position: fixed;
+    inset: 0;
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    z-index: -5;
+    pointer-events: none;
+  }
 
-      .animated-gradient {
-        position: fixed;
-        inset: 0;
-        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-        background-size: 400% 400%;
-        animation: gradient 15s ease infinite;
-        z-index: -5;
-        pointer-events: none;
-      }
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+</style>
 
-      @keyframes gradient {
-        0% {
-          background-position: 0% 50%;
-        }
-        50% {
-          background-position: 100% 50%;
-        }
-        100% {
-          background-position: 0% 50%;
-        }
-      }
-    </style>
-  </head>
-  <body>
-    <div class="animated-gradient"></div>
-  </body>
-</html>
+<div class="animated-gradient"></div>
 `;
+
+document.querySelector(".code-snippet").textContent = code;
 
 async function copyCode() {
   try {
@@ -49,6 +40,7 @@ async function copyCode() {
     console.error("Failed to copy: ", err);
   }
 }
+
 copyBtn.addEventListener("click", (e) => {
   copyCode();
   e.currentTarget.blur();
